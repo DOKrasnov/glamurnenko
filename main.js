eb50f40441dca920e67489654a4789e203ead241
Dmitriy Krasnov <krasnov.dmitry.o@gmail.com>
2022-12-05 20:56:58 +0300

    added fancybox-carousel,  jQuery

---
+++ main.js
@@ -0,0 +1,33 @@
+/////////////////////////////////
+// Sticky Header with Navbar
+
+const stickyHeaderObserver = new IntersectionObserver(
+  (entries) => {
+    const ent = entries[0];
+
+    if (ent.isIntersecting) {
+      $(".main-header").removeClass("sticky-top");
+    }
+    if (!ent.isIntersecting) {
+      $(".main-header").addClass("sticky-top");
+    }
+  },
+  {
+    root: null,
+    rootMargin: "-60px",
+  }
+);
+
+stickyHeaderObserver.observe($(".section-hero")[0]);
+
+///////////////////////////////////
+// Back to the top button
+
+$("#btn-to-top").click(() => {
+  $("html,body").animate(
+    {
+      scrollTop: 0,
+    },
+    800
+  );
+});
